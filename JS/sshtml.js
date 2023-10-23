5e35e6f3c99c9ef1665d6250467163660eb760db
WolfgangLoud <wolo8873@colorado.edu>
2023-10-23 16:06:58 -0600

    Lab #3

---
+++ JS/sshtml.js
@@ -0,0 +1,14 @@
+
+document.getElementById('fishForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    var name = document.getElementById('name').value;
+    var opinion = document.getElementById('opinion').value;
+    var tbody = document.getElementById('fishTable').getElementsByTagName('tbody')[0];
+    var newRow = tbody.insertRow();
+    var fishCell = newRow.insertCell();
+    var opinionCell = newRow.insertCell();
+    fishCell.textContent = name;
+    opinionCell.textContent = opinion;
+    document.getElementById('name').value = '';
+    document.getElementById('opinion').value = '';
+});
